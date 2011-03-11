b6521d0fd66f63d72999464c39a40d9037810222
Tatham Oddie <tatham@oddie.com.au>
2011-03-11 11:49:09 +1100

    Got basic multicharacter decoding working.

--- CrockfordBase32.Tests/CrockfordBase32EncodingTests.cs
+++ CrockfordBase32.Tests/CrockfordBase32EncodingTests.cs
@@ -48,5 +48,50 @@ namespace CrockfordBase32.Tests
 
             Assert.AreEqual(expected + checkDigit, actual);
         }
+
+        [TestMethod]
+        public void CrockfordBase32Encoding_Decode_ShouldThrowArgumentNullExceptionForNullInput()
+        {
+            try
+            {
+                new CrockfordBase32Encoding().Decode(null, false);
+                Assert.Fail("Expected exception was never thrown");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual("encodedString", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void CrockfordBase32Encoding_Decode_ShouldReturnNullForBadCharacter()
+        {
+            Assert.IsNull(new CrockfordBase32Encoding().Decode("/", false));
+        }
+
+        [TestMethod]
+        public void CrockfordBase32Encoding_Decode_ShouldReturnNullForBadCharacterWithinValidOtherwiseInput()
+        {
+            Assert.IsNull(new CrockfordBase32Encoding().Decode("a/b", false));
+        }
+
+        [TestMethod]
+        public void CrockfordBase32Encoding_Decode_ShouldReturnNullForEmptyString()
+        {
+            Assert.IsNull(new CrockfordBase32Encoding().Decode(string.Empty, false));
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"CrockfordBase32.Tests\TestData.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"|DataDirectory|\TestData.xml", "test", DataAccessMethod.Sequential)]
+        public void CrockfordBase32Encoding_Decode_ShouldReturnExpectedResult()
+        {
+            var encodedString = (string)TestContext.DataRow["encodedString"];
+            var expected = int.Parse((string)TestContext.DataRow["number"]);
+            
+            var actual = new CrockfordBase32Encoding().Decode(encodedString, false);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
--- CrockfordBase32/CrockfordBase32Encoding.cs
+++ CrockfordBase32/CrockfordBase32Encoding.cs
@@ -10,11 +10,13 @@ namespace CrockfordBase32
 
         static readonly IDictionary<int, char> valueEncodings;
         static readonly IDictionary<int, char> checkDigitEncodings;
+        static readonly IDictionary<char, int> valueDecodings;
         static CrockfordBase32Encoding()
         {
             var symbols = new SymbolDefinitions();
             valueEncodings = symbols.ValueEncodings;
             checkDigitEncodings = symbols.CheckDigitEncodings;
+            valueDecodings = symbols.ValueDecodings;
         }
 
         public string Encode(int number, bool includeCheckDigit)
@@ -47,6 +49,34 @@ namespace CrockfordBase32
             }
 
             return new string(((IEnumerable<char>)characters).Reverse().ToArray());
+        }
+
+        public int? Decode(string encodedString, bool treatLastCharacterAsCheckDigit)
+        {
+            if (treatLastCharacterAsCheckDigit)
+                throw new NotSupportedException();
+
+            if (encodedString == null)
+                throw new ArgumentNullException("encodedString");
+
+            if (encodedString.Length == 0)
+                return null;
+
+            var charactersInReverse = encodedString.Reverse().ToArray();
+
+            var number = 0;
+            var currentBase = 1;
+            foreach (var character in charactersInReverse)
+            {
+                if (!valueDecodings.ContainsKey(character)) return null;
+
+                var value = valueDecodings[character];
+                number += value*currentBase;
+
+                currentBase *= Base;
+            }
+
+            return number;
         }
     }
 }
