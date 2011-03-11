6092528e1c989100145b620e9a333ca1f2b4f9e4
Tatham Oddie <tatham@oddie.com.au>
2011-03-11 15:29:34 +1100

    Set version to 1.1.

--- CrockfordBase32/Properties/AssemblyInfo.cs
+++ CrockfordBase32/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: InternalsVisibleTo("CrockfordBase32.Tests")]
