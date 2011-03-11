d014e939075dc8a9eedbe654ce0ce26f02c17379
Tatham Oddie <tatham@oddie.com.au>
2011-03-11 11:13:22 +1100

    Removed the System.Text.Encoding base class for now as it's just adding excess complexity.

--- CrockfordBase32/CrockfordBase32Encoding.cs
+++ CrockfordBase32/CrockfordBase32Encoding.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CrockfordBase32
 {
-    public class CrockfordBase32Encoding : Encoding
+    public class CrockfordBase32Encoding
     {
         static readonly IDictionary<int, char> encodeMappings;
         static readonly IDictionary<char, int> decodeMappings;
@@ -15,34 +14,9 @@ namespace CrockfordBase32
             decodeMappings = symbols.DecodeMappings;
         }
 
-        public override int GetByteCount(char[] chars, int index, int count)
+        public string GetString(int number)
         {
-            throw new NotImplementedException();
-        }
-
-        public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetCharCount(byte[] bytes, int index, int count)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetMaxByteCount(int charCount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int GetMaxCharCount(int byteCount)
-        {
-            throw new NotImplementedException();
+            return new string(encodeMappings[number], 1);
         }
     }
 }
