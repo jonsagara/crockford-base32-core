524bee3e0ce524870d4e11113346f6c0bef271c4
Tatham Oddie <tatham@oddie.com.au>
2011-03-11 11:13:31 +1100

    Added a bunch of data driven unit tests.

--- CrockfordBase32.Tests/CrockfordBase32EncodingTests.cs
+++ CrockfordBase32.Tests/CrockfordBase32EncodingTests.cs
@@ -5,5 +5,19 @@ namespace CrockfordBase32.Tests
     [TestClass]
     public class CrockfordBase32EncodingTests
     {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DeploymentItem(@"CrockfordBase32.Tests\TestData.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"|DataDirectory|\TestData.xml", "test", DataAccessMethod.Sequential)]
+        public void CrockfordBase32Encoding_GetString_ShouldReturnExpectedResult()
+        {
+            var number = int.Parse((string)TestContext.DataRow["number"]);
+            var expected = (string)TestContext.DataRow["encodedString"];
+
+            var actual = new CrockfordBase32Encoding().GetString(number);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
