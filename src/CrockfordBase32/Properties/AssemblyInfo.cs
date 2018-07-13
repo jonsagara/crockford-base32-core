015f83b5c60932fcc503753d69dbf9def38e66c3
Jon Sagara <jon@sagarasoftware.com>
2018-07-12 17:46:11 -0700

    Fixed duplicate Assembly* attribute errors after converting the project to netstandard1.3.

--- src/CrockfordBase32/Properties/AssemblyInfo.cs
+++ src/CrockfordBase32/Properties/AssemblyInfo.cs
@@ -1,17 +1,7 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CrockfordBase32")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tatham Oddie")]
-[assembly: AssemblyProduct("CrockfordBase32")]
-[assembly: AssemblyCopyright("Copyright © Tatham Oddie 2011. Licensed under MS-PL.")]
 
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: InternalsVisibleTo("CrockfordBase32.Tests")]
