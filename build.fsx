fb87dc5181c2cba372f418829da1d39ea25a64f2
Jon Sagara <jon@sagarasoftware.com>
2018-08-01 11:21:30 -0700

    FAKE build script now builds the project, runs tests, and then creates a nuget package.

--- build.fsx
+++ build.fsx
@@ -1,4 +1,49 @@
 #r "paket:
-nuget Fake.Core.Target prerelease"
+nuget Fake.IO.FileSystem
+nuget Fake.DotNet.Cli
+nuget Fake.Core.Target //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
+#if !FAKE
+  #r "netstandard"
+#endif
+
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+
+
+// Targets
+Target.create "Clean" (fun _ ->
+    Shell.cleanDir "./artifacts/"
+)
+
+Target.create "BuildProject" (fun _ ->
+    DotNet.build (fun opt -> { opt with Configuration = DotNet.BuildConfiguration.Release }) ""
+)
+
+Target.create "RunTests" (fun _ ->
+    DotNet.test (fun opt -> { opt with Configuration = DotNet.BuildConfiguration.Release }) @".\src\CrockfordBase32.Tests.Core\CrockfordBase32.Tests.Core.csproj"
+)
+
+Target.create "CreateNuGetPackage" (fun _ ->
+
+    let setOptions (opt:DotNet.PackOptions) =
+        { opt with 
+            Configuration = DotNet.BuildConfiguration.Release
+            NoBuild = true
+            OutputPath = Some "../../artifacts/" }
+
+    DotNet.pack setOptions "./src/CrockfordBase32/CrockfordBase32.csproj"
+)
+
+
+// Dependencies
+"Clean"
+    ==> "BuildProject"
+    ==> "RunTests"
+    ==> "CreateNuGetPackage"
+
+// Start build
+Target.runOrDefault "CreateNuGetPackage"
